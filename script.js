9108b098c489acdd566676f8f023a756c0ee7785
bekzhanJS <bekzhanzhumassatov@gmail.com>
2024-03-21 00:21:55 +0500

    Added functionality

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const btn = document.getElementById("btn");
 
+let generate = () => {
+  const API = "e8sda3cothhKwTUjeRf2nRjMJ1slnoS5";
+  const input = document.getElementById("input").value;
+  const show = document.getElementById("show");
+
+  let URL = `http://api.giphy.com/v1/gifs/search?api_key=${API}&limit=20&q=${input}$limit=20&rating=g`;
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((result) => {
+      let gifs = result.data;
+      console.log(gifs);
+
+      show.innerHTML = "";
+
+      gifs.forEach((gif) => {
+        let img = document.createElement("img");
+        img.setAttribute("src", gif.images.downsized_medium.url);
+
+        show.append(img);
+        console.log(show);
+      });
+    })``.catch((error) => {
+    console.error(error);
+  });
+};
+
+btn.addEventListener("click", generate);
+window.addEventListener("load", generate);
